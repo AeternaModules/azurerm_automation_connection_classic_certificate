7d46dc7cdca306ca97103c0e3437fe173eab548d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:54:52 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_connection_classic_certificates" {
+  description = "All automation_connection_classic_certificate resources"
+  value       = azurerm_automation_connection_classic_certificate.automation_connection_classic_certificates
+}
+
